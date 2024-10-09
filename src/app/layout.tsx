90b3e940be0d9b74c5f5b4507cc681a3a018981f
Naman Soni <namantsoni@gmail.com>
2024-10-09 10:49:45 +0530

    Sign In and Sign Out functionality using Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans"});
 
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("min-h-screen, bg-background, font-sans antialiased" ,inter.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("min-h-screen, bg-background, font-sans antialiased" ,inter.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
