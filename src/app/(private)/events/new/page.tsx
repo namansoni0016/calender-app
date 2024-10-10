19bb12f2fcd9bc558d265d36cf10f8aeb35b433e
Naman Soni <namantsoni@gmail.com>
2024-10-10 12:03:49 +0530

    Event Page and NavLink Component

---
+++ src/app/(private)/events/new/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function NewEventPage() {
+    return (
+        <Card className="max-w-md mx-auto">
+            <CardHeader>
+                <CardTitle>New Event</CardTitle>
+            </CardHeader>
+            <CardContent>
+                content
+            </CardContent>
+        </Card>
+    )
+}
