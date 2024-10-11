fca5afbeff32c9d254c82785ca5bf11ad8f80ec1
Naman Soni <namantsoni@gmail.com>
2024-10-11 12:45:51 +0530

    Displaying events

--- src/app/(private)/events/page.tsx
+++ src/app/(private)/events/page.tsx
@@ -4,6 +4,10 @@ import { Button } from "@/components/ui/button";
 import { db } from "@/drizzle/db";
 import { auth } from "@clerk/nextjs/server";
 import { CalendarRange } from "lucide-react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { formatEventDescription } from "@/lib/formatters";
+import { CopyEventButton } from "@/components/CopyEventButton";
+import { cn } from "@/lib/utils";
 
 export default async function EventsPage() {
     const { userId, redirectToSignIn } = auth();
@@ -20,7 +24,12 @@ export default async function EventsPage() {
                     <Link href="/events/new"><CalendarPlus className="mr-4 size-6"/> New Event</Link>
                 </Button>
             </div>
-            {events.length > 0 ? (<h1>Events</h1>) : (
+            {events.length > 0 ? (
+                <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(400px,1fr))]">
+                    {events.map(event => (
+                        <EventCard key={event.id} {...event} />
+                    ))}
+                </div>) : (
                 <div className="flex flex-col items-center gap-4">
                     <CalendarRange className="size-16 mx-auto"/>
                     You do not have any events yet. Create your first event to get started!
@@ -30,5 +39,38 @@ export default async function EventsPage() {
                 </div>
             )}
         </>
+    )
+}
+
+type EventCardProps = {
+    id: string,
+    isActive: boolean,
+    name: string,
+    description: string | null,
+    durationInMinutes: number,
+    clerkUserId: string,
+}
+
+function EventCard({
+    id,
+    isActive,
+    name, description, durationInMinutes, clerkUserId
+}: EventCardProps) {
+    return (
+        <Card className={cn("flex, flex-col", !isActive && "border-secondary/50")}>
+            <CardHeader className={cn(!isActive && "opacity-50")}>
+                <CardTitle>{name}</CardTitle>
+                <CardDescription>{formatEventDescription(durationInMinutes)}</CardDescription>
+            </CardHeader>
+            {description != null && (
+                <CardContent className={cn(!isActive && "opacity-50")}>{description}</CardContent>
+            )}
+            <CardFooter className="flex justify-end gap-2 mt-auto ">
+                {isActive && (<CopyEventButton variant="outline" eventId={id} clerkUserId={clerkUserId}/>)}
+                <Button asChild>
+                    <Link href={`/events/${id}/edit`}>Edit</Link>
+                </Button>
+            </CardFooter>
+        </Card>
     )
 }
