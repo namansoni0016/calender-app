d47e257660b8150cc02d6b3a1ac2640340060342
Naman Soni <namantsoni@gmail.com>
2024-10-15 10:51:36 +0530

    Google calender integration

--- src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx
+++ src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx
@@ -1,3 +1,69 @@
-export default function BookEventPage() {
-    return <h1>Hi</h1>
+import { db } from "@/drizzle/db"
+import { clerkClient } from "@clerk/nextjs/server";
+import { addMonths, eachMinuteOfInterval, endOfDay, roundToNearestMinutes } from "date-fns";
+import { notFound } from "next/navigation";
+import { getValidTimesFromSchedule } from "@/lib/getValidTimesFromSchedule";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function BookEventPage({ params: { clerkUserId, eventId }}: { params: { clerkUserId: string, eventId: string }}) {
+    const event = await db.query.EventTable.findFirst({
+        where: ({clerkUserId: userIdCol, isActive, id }, { eq, and }) => and(eq(isActive, true), eq(userIdCol, clerkUserId), eq(id, eventId)),
+    });
+    if(event == null) return notFound();
+    const calenderUser = await clerkClient().users.getUser(clerkUserId);
+    const startDate = roundToNearestMinutes(new Date(), {
+        nearestTo: 15, roundingMethod: "ceil"
+    });
+    const endDate = endOfDay(addMonths(startDate, 2));
+    const validTimes = await getValidTimesFromSchedule(eachMinuteOfInterval({ start: startDate, end: endDate }, {
+        step: 15
+    }), event)
+    if(validTimes.length === 0) {
+        return <NoTimeSlots event={event} calenderUser={calenderUser} />
+    }
+    return (
+        <Card className="max-w-4xl mx-auto">
+            <CardHeader>
+                <CardTitle>
+                    Book {event.name} with {calenderUser.fullName}
+                </CardTitle>
+                {event.description && (
+                    <CardDescription>{event.description}</CardDescription>
+                )}
+            </CardHeader>
+            <CardContent>
+                <MeetingForm validTimes={validTimes} eventId={event.id} clerkUserId={clerkUserId}/>
+            </CardContent>
+        </Card>
+    )
+}
+
+function NoTimeSlots({ event, calenderUser }: { 
+    event: { name: string; description: string | null}
+    calenderUser: { id: string, fullName: string | null }
+}) {
+    return (
+        <Card className="max-w-md mx-auto">
+            <CardHeader>
+                <CardTitle>
+                    Book {event.name} with {calenderUser.fullName}
+                </CardTitle>
+                {event.description && (
+                    <CardDescription>{event.description}</CardDescription>
+                )}
+            </CardHeader>
+            <CardContent>
+                {calenderUser.fullName} is currently booked up. Please check back later or choose a shorter event.
+            </CardContent>
+            <CardFooter>
+                <Button asChild>
+                    <Link href={`/book/${calenderUser.id}`}>
+                        Choose Another Event
+                    </Link>
+                </Button>
+            </CardFooter>
+        </Card>
+    )
 }
---
+++ src/server/googleCalender.ts
@@ -0,0 +1,42 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { google } from "googleapis";
+import { endOfDay, startOfDay } from "date-fns";
+import "use-server";
+
+export async function getCalenderEventTimes(clerkUserId: string, {start, end}: { start: Date, end: Date }) {
+    const oAuthClient = await getOAuthClient(clerkUserId);
+    const events = await google.calendar("v3").events.list({
+        calendarId: "primary",
+        eventTypes: ["default"],
+        singleEvents: true,
+        timeMin: start.toISOString(),
+        timeMax: end.toISOString(),
+        maxResults: 2500,
+        auth: oAuthClient
+    });
+    return events.data.items?.map(event => {
+        if (event.start?.date != null && event.end?.date != null){
+            return {
+                start: startOfDay(event.start.date),
+                end: endOfDay(event.end.date)
+            }
+        }
+        if(event.start?.dateTime != null && event.end?.dateTime != null) {
+            return {
+                start: new Date(event.start.dateTime),
+                end: new Date(event.end.dateTime)
+            }
+        }
+    }).filter(date => date != null) || []
+}
+
+async function getOAuthClient(clerkUserId: string) {
+    console.log(clerkUserId)
+    const token = await clerkClient().users.getUserOauthAccessToken(clerkUserId, "oauth_google");
+    if(token.data.length === 0 || token.data[0].token == null) {
+        return;
+    }
+    const client = new google.auth.OAuth2(process.env.GOOGLE_OAUTH_CLIENT_ID, process.env.GOOGLE_OAUTH_CLIENT_SECRET, process.env.GOOGLE_OAUTH_REDIRECT_URL)
+    client.setCredentials({ access_token: token.data[0].token})
+    return client;
+}
